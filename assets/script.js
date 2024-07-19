754852999db4741341d62ab9398d8ad623346734
unknown <rabesonandreas@gmail.com>
2024-07-19 12:39:13 +0200

    Mise à jour du portfolio

--- assets/script.js
+++ assets/script.js
@@ -30,23 +30,72 @@ const nom = document.getElementById("nom");
 const prenom = document.getElementById("prenom");
 const btn = document.querySelector('.confirm-btn');
 
-// btn.addEventListener('click', afficherPopup);
-// function afficherPopup(){  
-//     if(nom.value !== '' && prenom.value !== '') {
-//         popup.classList.add("popup-ouvert");
-//         nom.value = "";
-//         prenom.value = "";
-//     }
-//     else{
-//         alert('Remplissez le formulaire correctement svp!')
-//     }
+// Gallery d'image
+// const displayedImage = document.querySelector('.displayed-img');
+// const thumbBar = document.querySelector('.thumb-bar');
 
+// const btn = document.querySelector('button');
+// const display = document.querySelector('.displayed-img');
+// const overlay = document.querySelector('.overlay');
+
+// /* Declaring the array of image filenames */
+
+// const tab_img = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
+// const tab_object = [
+//     {nom:'pic1.jpg', description:'Photo d\'un oeil'},
+//     {nom:'pic2.jpg', description:'Photo bizarre'},
+//     {nom:'pic3.jpg', description:'Photo de fleurs'},
+//     {nom:'pic4.jpg', description:'Photo des Egyptiens'},
+//     {nom:'pic5.jpg', description:'Photo d\'un papillon'},
+// ];
+
+// /* Declaring the alternative text for each image file */
+// for (const photos of tab_object){
+//     const image = document.createElement('img');
     
-//     const t = document.querySelector('.popup-ouvert');
-//     if(t !== null){
-//         popup.addEventListener('click', () => {
-//             popup.classList.remove("popup-ouvert");
-//         });
-//     }
+//     image.setAttribute('src', `img/${photos.nom}`);
+//     image.setAttribute('alt', `${photos.description}`);
+
+//     thumbBar.appendChild(image);
 // }
 
+// thumbBar.addEventListener("click", (event) => {
+//     const src = event.target.getAttribute('src');
+//     const alt = event.target.getAttribute('alt');
+    
+//     display.setAttribute('src',src);
+//     display.setAttribute('alt',alt);
+// });
+
+// /* Wiring up the Darken/Lighten button */
+
+// btn.addEventListener("click", () => {
+//     if(btn.textContent === 'Dark'){
+//         btn.textContent = 'Light';
+//         display.setAttribute('class','dark')
+//         overlay.style.backgroundColor = 'rgb(0,0,0,0.5)';
+//     }
+//     else{
+//         btn.textContent = 'Dark';
+//         display.removeAttribute('class','dark')
+//         overlay.style.backgroundColor = 'rgb(0,0,0,0)';
+//     }
+// });
+
+
+// Gallery d'image
+
+
+function changeImage(buttonId) {
+    const imageElement = document.querySelector('displayed-img');
+    
+    switch(buttonId) {
+        case 'Wps':
+            imageElement.src = '../img/Portfolio/jadechecking.png';
+            break;
+        default:
+            imageElement.src = '../img/Portfolio/gr.png';
+    }
+    alert(success);
+}

36217ee083821141dc6c6ee1410d29871995d33e
unknown <rabesonandreas@gmail.com>
2024-07-22 13:06:54 +0200

    Ajout de la popup Modal

--- assets/script.js
+++ assets/script.js
@@ -111,3 +111,29 @@ function changeImage(idProjet) {
 // Ajoutez un écouteur d'événement pour le clic
 voir_gr.addEventListener('click', () => changeImage("gr"));
 voir_jd.addEventListener('click', () => changeImage("jd"));
+
+// Modal 
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.querySelectorAll(".myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+
+
+images.forEach(function(img) {
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}

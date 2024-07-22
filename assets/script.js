02f36f497ac4843505573575e54704e07e9401e3
unknown <rabesonandreas@gmail.com>
2024-07-22 08:53:30 +0200

    Mise à jour des projets

--- assets/script.js
+++ assets/script.js
@@ -23,6 +23,23 @@ if (targetElement) {
 }
 }
 
+    /**
+     * Animate the skills items on reveal
+     */
+    let skillsAnimation = document.querySelectorAll('.skills-animation');
+    skillsAnimation.forEach((item) => {
+    new Waypoint({
+        element: item,
+        offset: '80%',
+        handler: function(direction) {
+        let progress = item.querySelectorAll('.progress .progress-bar');
+        progress.forEach(el => {
+            el.style.width = el.getAttribute('aria-valuenow') + '%';
+        });
+        }
+    });
+    });
+
 // Popup
 const popup = document.getElementById("popup");
 const form = document.getElementById("formulaire-contact");
@@ -30,70 +47,67 @@ const nom = document.getElementById("nom");
 const prenom = document.getElementById("prenom");
 const btn = document.querySelector('.confirm-btn');
 
-// Gallery d'image
-// const displayedImage = document.querySelector('.displayed-img');
-// const thumbBar = document.querySelector('.thumb-bar');
-
-// const btn = document.querySelector('button');
-// const display = document.querySelector('.displayed-img');
-// const overlay = document.querySelector('.overlay');
-
-// /* Declaring the array of image filenames */
-
-// const tab_img = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
-
-// const tab_object = [
-//     {nom:'pic1.jpg', description:'Photo d\'un oeil'},
-//     {nom:'pic2.jpg', description:'Photo bizarre'},
-//     {nom:'pic3.jpg', description:'Photo de fleurs'},
-//     {nom:'pic4.jpg', description:'Photo des Egyptiens'},
-//     {nom:'pic5.jpg', description:'Photo d\'un papillon'},
-// ];
-
-// /* Declaring the alternative text for each image file */
-// for (const photos of tab_object){
-//     const image = document.createElement('img');
-    
-//     image.setAttribute('src', `img/${photos.nom}`);
-//     image.setAttribute('alt', `${photos.description}`);
-
-//     thumbBar.appendChild(image);
-// }
-
-// thumbBar.addEventListener("click", (event) => {
-//     const src = event.target.getAttribute('src');
-//     const alt = event.target.getAttribute('alt');
-    
-//     display.setAttribute('src',src);
-//     display.setAttribute('alt',alt);
-// });
-
-// /* Wiring up the Darken/Lighten button */
-
-// btn.addEventListener("click", () => {
-//     if(btn.textContent === 'Dark'){
-//         btn.textContent = 'Light';
-//         display.setAttribute('class','dark')
-//         overlay.style.backgroundColor = 'rgb(0,0,0,0.5)';
-//     }
-//     else{
-//         btn.textContent = 'Dark';
-//         display.removeAttribute('class','dark')
-//         overlay.style.backgroundColor = 'rgb(0,0,0,0)';
-//     }
-// });
 
 
 // Gallery d'image
+const voir_gr = document.getElementById('voir-gallerie-gr');
+const voir_jd = document.getElementById('voir-gallerie-jd');
 
-function changeImage(buttonId) {
-    const imageElement = document.getElementById('displayed-img');
-    
-    // if (buttonId == 'Wps') {
-    //     imageElement.src = '../img/Portfolio/jadechecking.png';
-    // }
-    // else {
-    //     imageElement.src = '../img/Portfolio/gr.png';
-    // }
-    console.log(buttonId);
+const gallerie_gr = document.getElementById('gallerie-gr');
+const gallerie_jd = document.getElementById('gallerie-jd');
+
+const para_gr = document.getElementById('para-gr');
+const para_jd = document.getElementById('para-jd');
+
+
+function changeImage(idProjet) {
+    if (idProjet==="gr"){
+        if (gallerie_gr.classList.contains('hidden')) {
+            para_gr.classList.remove('fade-in');
+            para_gr.classList.add('fade-out');
+            setTimeout(() => {
+                para_gr.classList.add('hidden');
+                gallerie_gr.classList.remove('hidden');
+                gallerie_gr.classList.remove('fade-out');
+                gallerie_gr.classList.add('fade-in');
+            }, 1000); // Durée de l'animation en millisecondes
+        } else {
+            gallerie_gr.classList.remove('fade-in');
+            gallerie_gr.classList.add('fade-out');
+            setTimeout(() => {
+                gallerie_gr.classList.add('hidden');
+                para_gr.classList.remove('hidden');
+                para_gr.classList.remove('fade-out');
+                para_gr.classList.add('fade-in');
+            }, 1000); // Durée de l'animation en millisecondes
+        }
+    }
+    else if (idProjet==="jd"){
+        if (gallerie_jd.classList.contains('hidden')) {
+            para_jd.classList.remove('fade-in');
+            para_jd.classList.add('fade-out');
+            setTimeout(() => {
+                para_jd.classList.add('hidden');
+                gallerie_jd.classList.remove('hidden');
+                gallerie_jd.classList.remove('fade-out');
+                gallerie_jd.classList.add('fade-in');
+            }, 1000); // Durée de l'animation en millisecondes
+        } else {
+            gallerie_jd.classList.remove('fade-in');
+            gallerie_jd.classList.add('fade-out');
+            setTimeout(() => {
+                gallerie_jd.classList.add('hidden');
+                para_jd.classList.remove('hidden');
+                para_jd.classList.remove('fade-out');
+                para_jd.classList.add('fade-in');
+            }, 1000); // Durée de l'animation en millisecondes
+        }
+    }
+    else {
+
+    }
 }
+
+// Ajoutez un écouteur d'événement pour le clic
+voir_gr.addEventListener('click', () => changeImage("gr"));
+voir_jd.addEventListener('click', () => changeImage("jd"));
